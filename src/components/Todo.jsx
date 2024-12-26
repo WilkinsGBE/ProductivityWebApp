73728ecabae691a812e823ff29c06b6410ad3d76
WilkinsGBE <saintilwilkins@gmail.com>
2024-12-26 12:01:00 -0500

    Version 1 of the finish gui of the todo app

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import "../style/TodoApp.css";
+import todo_icon from "../assets/todo_icon.png";
+import TodoItems from "./TodoItems";
 
 export const Todo = () => {
   return (
     <>
       <div className="body">
         <div className="box">
+          {/* -------- Title ------- */}
           <div className="header">
+            <img src={todo_icon} alt="" />
             <h1>To-Do List</h1>
+          </div>
+          {/* ------ Input box ------ */}
+          <div className="input">
+            <input type="text" placeholder="Add your task" />
+            <button>ADD</button>
+          </div>
+
+          {/* ----- To-do list --------- */}
+          <div className="todo-items">
+          <TodoItems text="Learn Coding"/>
+          <TodoItems text="Learn App Development"/>
           </div>
         </div>
       </div>
---
+++ src/components/TodoItems.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../style/TodoApp.css";
+import tick from "../assets/tick.png";
+import not_tick from "../assets/not_tick.png";
+import delete_icon from "../assets/delete.png";
+
+const TodoItems = ({text}) => {
+  return (
+    <>
+      <div className="todolist">
+        <div className="list">
+          <img src={tick} alt="" />
+          <p>{text}</p>
+        </div>
+        <img className="delete-icon " src={delete_icon} alt="" />
+      </div>
+    </>
+  );
+};
+
+export default TodoItems;
