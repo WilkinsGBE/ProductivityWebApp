80c04c4e57f72ac659f694511b1f84140087ca13
WilkinsGBE <saintilwilkins@gmail.com>
2025-01-04 19:07:19 -0500

    Fix the nav styling + the margin issue. The CSS is in TodoApp.css (unfortunately)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { Todo } from './components/TodoApp/Todo';
-import { Homepage } from './components/Homepage/Homepage';
-import { Pomodoro } from './components/PomodoroApp/Pomodoro';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Todo } from "./components/TodoApp/Todo";
+import { Homepage } from "./components/Homepage/Homepage";
+import { Pomodoro } from "./components/PomodoroApp/Pomodoro";
 
 const App = () => {
   return (
@@ -10,28 +10,33 @@ const App = () => {
       <div>
         <nav>
           <ul>
-            <li>
+            <li>Productivity Web App</li>
+            <li className="home">
               <Link to="/">Homepage</Link>
             </li>
-            <li>
+            <li className="todos">
               <Link to="/todo">To-Do</Link>
             </li>
-           
-             <li>
-              <Link to="/pomodoro">Pomodoro</Link>
-            </li> 
             {/* Uncomment and add these when the components are ready */}
-            {/*<li>
+            {/*<li className="note">
               <Link to="/notes">Notes</Link>
             </li> */}
+            <li className="pomo">
+              <Link to="/pomodoro">Pomodoro</Link>
+            </li>
+            <li>Instagram icon</li>
+            <li>LinkeDIn icon</li>
+            <li>GitHub Icon</li>
+            <li>Youtube Icon ?</li>
           </ul>
         </nav>
         <Routes>
           {/* Set the Homepage as the default path */}
           <Route path="/" element={<Homepage />} />
-          <Route path="/todo" element={<Todo />} />
-          <Route path="/pomodoro" element={<Pomodoro />} />
+          <Route path="/todo" element={<Todo />} />         
           {/*<Route path="/notes" element={<Notes />} /> */}
+          <Route path="/pomodoro" element={<Pomodoro />} />
+
         </Routes>
       </div>
     </Router>
