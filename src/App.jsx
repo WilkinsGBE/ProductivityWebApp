5adb52ebc95deafefc4f59e55bee59b339a7f3d6
WilkinsGBE <saintilwilkins@gmail.com>
2025-01-07 22:09:02 -0500

    installed new packages for two more icons and created the functions open and close tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import { Todo } from "./components/TodoApp/Todo";
 import { Homepage } from "./components/Homepage/Homepage";
 import { Pomodoro } from "./components/PomodoroApp/Pomodoro";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faInstagram,
-  faLinkedin,
-  faGithub,
-} from "@fortawesome/free-brands-svg-icons";
+import { faInstagram, faLinkedin, faGithub} from "@fortawesome/free-brands-svg-icons";
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 const App = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const openmenu = () => {
+    setMenuOpen(true); // Show the menu
+  };
+
+  const closemenu = () => {
+    setMenuOpen(false); // Hide the menu
+  };
+
   return (
     <Router>
       <div>
         <nav>
+        <i class={faXmark} onclick="closemenu()"></i>
           <ul>
             <li>Productivity Web App</li>
             <li className="home">
@@ -43,6 +51,7 @@ const App = () => {
               <FontAwesomeIcon icon={faInstagram} />
             </a>
           </ul>
+          <FontAwesomeIcon icon={faBars} size="2x" onClick={openmenu} />
         </nav>
         <Routes>
           {/* Set the Homepage as the default path */}
