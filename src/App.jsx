3bad6433fcaaf1d224080d849b16051add200220
WilkinsGBE <saintilwilkins@gmail.com>
2025-01-07 21:54:12 -0500

    installed font awesome for the different icon i need in the nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import { Todo } from "./components/TodoApp/Todo";
 import { Homepage } from "./components/Homepage/Homepage";
 import { Pomodoro } from "./components/PomodoroApp/Pomodoro";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faLinkedin,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 
 const App = () => {
   return (
@@ -24,19 +30,26 @@ const App = () => {
             <li className="pomo">
               <Link to="/pomodoro">Pomodoro</Link>
             </li>
-            <li>Instagram icon</li>
-            <li>LinkeDIn icon</li>
-            <li>GitHub Icon</li>
-            <li>Youtube Icon ?</li>
+            <a
+              href="https://www.linkedin.com/in/ariel-wilkins-saintil-a79207286/"
+              target="_blank"
+            >
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+            <a href="https://github.com/WilkinsGBE" target="_blank">
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+            <a href="https://www.instagram.com/wilkinss.1/" target="_blank">
+              <FontAwesomeIcon icon={faInstagram} />
+            </a>
           </ul>
         </nav>
         <Routes>
           {/* Set the Homepage as the default path */}
           <Route path="/" element={<Homepage />} />
-          <Route path="/todo" element={<Todo />} />         
+          <Route path="/todo" element={<Todo />} />
           {/*<Route path="/notes" element={<Notes />} /> */}
           <Route path="/pomodoro" element={<Pomodoro />} />
-
         </Routes>
       </div>
     </Router>
