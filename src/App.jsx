0fce914a2671e03828c66b22f27997b6a43c367a
WilkinsGBE <saintilwilkins@gmail.com>
2025-01-10 12:06:26 -0500

    Changed the backend of the header, I moved everything in a new file header

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,17 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Header } from "./Header";
 import { Todo } from "./components/TodoApp/Todo";
 import { Homepage } from "./components/Homepage/Homepage";
-import { Pomodoro } from "./components/PomodoroApp/Pomodoro";
 import { NotesApp } from "./components/NoteApp/NotesApp";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faInstagram,
-  faLinkedin,
-  faGithub,
-} from "@fortawesome/free-brands-svg-icons";
-import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { Pomodoro } from "./components/PomodoroApp/Pomodoro";
 
 const App = () => {
-  const [isMenuOpen, setMenuOpen] = useState(false);
-
-  const openmenu = () => {
-    setMenuOpen(true); // Show the menu
-  };
-
-  const closemenu = () => {
-    setMenuOpen(false); // Hide the menu
-  };
-
   return (
     <Router>
-      <div>
-        <nav>
-          {/* App Title - always visible */}
-          <div className="title">Productivity Web App</div>
-
-          {/* "Bars" icon for opening the menu */}
-          {!isMenuOpen && (
-            <FontAwesomeIcon
-              icon={faBars}
-              size="2x"
-              onClick={openmenu}
-              className={`open-menu-icon ${isMenuOpen ? "open" : ""}`}
-            />
-          )}
-
-          {/* Menu Container */}
-          {isMenuOpen && (
-            <div className={`menu-container ${isMenuOpen ? "open" : ""}`}>
-              {/* Close icon */}
-              <FontAwesomeIcon
-                icon={faXmark}
-                size="2x"
-                onClick={closemenu}
-                className="close-menu-icon"
-              />
-
-              <ul>
-                <li className="home">
-                  <Link to="/">Homepage</Link>
-                </li>
-                <li className="todos">
-                  <Link to="/todo">To-Do</Link>
-                </li>
-                <li className="notes">
-                  <Link to="/notes">Notes</Link>
-                </li>
-                <li className="pomo">
-                  <Link to="/pomodoro">Pomodoro</Link>
-                </li>
-              </ul>
-              <div className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/ariel-wilkins-saintil-a79207286/"
-                  target="_blank"
-                >
-                  <FontAwesomeIcon icon={faLinkedin} size="2x" />
-                </a>
-                <a href="https://github.com/WilkinsGBE" target="_blank">
-                  <FontAwesomeIcon icon={faGithub} size="2x" />
-                </a>
-                <a href="https://www.instagram.com/wilkinss.1/" target="_blank">
-                  <FontAwesomeIcon icon={faInstagram} size="2x"/>
-                </a>
-              </div>
-            </div>
-          )}
-        </nav>
+      <Header /> {/* Always visible header with navigation links */}
+      <div className="content">
         <Routes>
-          {/* Set the Homepage as the default path */}
           <Route path="/" element={<Homepage />} />
           <Route path="/todo" element={<Todo />} />
           <Route path="/notes" element={<NotesApp />} />
