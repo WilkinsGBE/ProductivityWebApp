4e85b1ae692b7509dff24eecb5ac6afec77801e5
WilkinsGBE <saintilwilkins@gmail.com>
2025-01-22 12:08:22 -0500

    added a function that closes the menu when you click on an app link

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  useNavigate,
+} from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faInstagram,
@@ -12,6 +18,7 @@ import "./components/TodoApp/TodoApp.css";
 export const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const handleResize = () => {
@@ -29,11 +36,18 @@ export const Header = () => {
   const openMenu = () => setIsMenuOpen(true);
   const closeMenu = () => setIsMenuOpen(false);
 
+  const handleLinkClick = (to) => {
+    closeMenu();
+    setTimeout(() => {
+      navigate(to);
+    }, 300);
+  };
+
   return (
     <div className="Main-header">
       <img
         className="icon"
-        src="/ProductivityWebApp/public/Productivity-Web-App-icon.png"
+        href="/Productivity-Web-App-icon.png"
         alt="App icon"
       />
 
@@ -61,16 +75,24 @@ export const Header = () => {
 
           <ul>
             <li className="home">
-              <Link to="/homepage">Homepage</Link>
+              <Link to="/homepage" onClick={() => handleLinkClick("/homepage")}>
+                Homepage{" "}
+              </Link>
             </li>
             <li className="todos">
-              <Link to="/todo">To-Do</Link>
+              <Link to="/todo" onClick={() => handleLinkClick("/todo")}>
+                To-Do
+              </Link>
             </li>
             <li className="notes">
-              <Link to="/notes">Notes</Link>
+              <Link to="/notes" onClick={() => handleLinkClick("/notes")}>
+                Notes
+              </Link>
             </li>
             <li className="pomo">
-              <Link to="/pomodoro">Pomodoro</Link>
+              <Link to="/pomodoro" onClick={() => handleLinkClick("/pomodoro")}>
+                Pomodoro
+              </Link>
             </li>
           </ul>
           <div className="social-icons">
@@ -79,30 +101,21 @@ export const Header = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <FontAwesomeIcon
-                icon={faLinkedin}
-                size="3x"
-              />
+              <FontAwesomeIcon icon={faLinkedin} size="3x" />
             </a>
             <a
               href="https://github.com/WilkinsGBE"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <FontAwesomeIcon
-                icon={faGithub}
-                size="3x"
-              />
+              <FontAwesomeIcon icon={faGithub} size="3x" />
             </a>
             <a
               href="https://www.instagram.com/wilkinss.1/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <FontAwesomeIcon
-                icon={faInstagram}
-                size="3x"
-              />
+              <FontAwesomeIcon icon={faInstagram} size="3x" />
             </a>
           </div>
         </div>
